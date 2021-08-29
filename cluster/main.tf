9acab3788733cdecc16fa42374006c9b21a6e3c2
skyer9 <skyer9@gmail.com>
2021-08-29 21:30:04 +0900

    Code Alignment

--- cluster/main.tf
+++ cluster/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "consul_server" {
-  ami           = var.ami
-  instance_type = var.server_instance_type
+  ami                    = var.ami
+  instance_type          = var.server_instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [data.aws_security_group.primary.id]
   count                  = var.consul_server_count
 
   tags = {
-    Name = "${var.stack_name}-consul_server"
+    Name           = "${var.stack_name}-consul_server"
     ConsulAutoJoin = "auto-join"
     OwnerName      = var.owner_name
     OwnerEmail     = var.owner_email
