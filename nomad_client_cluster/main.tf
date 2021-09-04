2f6df8712f29f83c50af6c8e6a8cc3820b5071a4
skyer9 <skyer9@gmail.com>
2021-09-04 17:39:17 +0900

    add nomad cluster

--- nomad_client_cluster/main.tf
+++ nomad_client_cluster/main.tf
@@ -2,16 +2,15 @@ provider "aws" {
   region  = var.region
 }
 
-resource "aws_instance" "consul_server" {
+resource "aws_instance" "nomad_client" {
   ami                    = var.ami
   instance_type          = var.server_instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [data.aws_security_group.primary.id]
-  count                  = var.consul_server_count
+  count                  = var.client_count
 
   tags = {
-    Name           = "${var.stack_name}-consul_server"
-    ConsulAutoJoin = "auto-join"
+    Name           = "${var.stack_name}-nomad_client"
     OwnerName      = var.owner_name
     OwnerEmail     = var.owner_email
   }
@@ -22,5 +21,5 @@ resource "aws_instance" "consul_server" {
     delete_on_termination = "true"
   }
 
-  user_data            = data.template_file.user_data_consul_server.rendered
+  user_data            = data.template_file.user_data_nomad_client.rendered
 }
--- nomad_client_cluster/templates.tf
+++ nomad_client_cluster/templates.tf
@@ -1,8 +1,8 @@
-data "template_file" "user_data_consul_server" {
-  template = file("${path.module}/files/user-data-consul-server.sh")
+data "template_file" "user_data_nomad_client" {
+  template = file("${path.module}/files/user-data-nomad-client.sh")
 
   vars = {
-    server_count  = var.consul_server_count
+    server_count  = var.client_count
     region        = var.region
     retry_join    = var.retry_join
   }
