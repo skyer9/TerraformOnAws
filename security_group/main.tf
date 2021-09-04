e57bcc4bbc6b74c3c46d9396d794b53f5ab76dc8
skyer9 <skyer9@gmail.com>
2021-09-04 21:39:19 +0900

    add nomad cluster

--- security_group/main.tf
+++ security_group/main.tf
@@ -6,3 +6,23 @@ resource "aws_iam_instance_profile" "consul_profile" {
   name = "${var.stack_name}-consul_profile"
   role = "DescribeInstancesRole"
 }
+
+resource "aws_iam_role" "role" {
+  name = "test_role"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
