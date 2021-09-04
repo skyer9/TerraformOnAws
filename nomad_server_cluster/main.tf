bef4bf1b58ed7444bd0841a640ecfff72d6945c3
skyer9 <skyer9@gmail.com>
2021-09-04 18:57:06 +0900

    add nomad cluster

--- nomad_server_cluster/main.tf
+++ nomad_server_cluster/main.tf
@@ -2,12 +2,41 @@ provider "aws" {
   region  = var.region
 }
 
+resource "aws_iam_role" "consul_role" {
+  name = "consul_role"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeInstances"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_instance_profile" "consul_profile" {
+  name = "consul_profile"
+  role = aws_iam_role.consul_role.name
+}
+
 resource "aws_instance" "nomad_server" {
   ami                    = var.ami
   instance_type          = var.server_instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [data.aws_security_group.primary.id]
   count                  = var.server_count
+  iam_instance_profile   = "consul_profile"
 
   tags = {
     Name           = "${var.stack_name}-nomad_server"
