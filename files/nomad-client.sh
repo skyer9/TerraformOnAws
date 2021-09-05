8a0a8d7281ecd4ad0e1d072d590c9304a95d8fe9
skyer9 <skyer9@gmail.com>
2021-09-05 10:17:50 +0900

    add nomad cluster

--- files/nomad-client.sh
+++ files/nomad-client.sh
@@ -18,7 +18,7 @@ RETRY_JOIN=$2
 
 # Consul
 
-sed -i "s/RETRY_JOIN/$RETRY_JOIN/g" $FILES_DIR/consul-client.hcl
+sed -i "s/RETRY_JOIN/$RETRY_JOIN/g" $FILES_DIR/consul.service
 sudo cp $FILES_DIR/consul-client.hcl $CONSUL_CONFIG_DIR/consul.hcl
 sudo cp $FILES_DIR/consul.service /etc/systemd/system/consul.service
 
