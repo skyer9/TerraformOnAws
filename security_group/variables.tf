e7764cc8c8074a0eaf922dc7a834fd6cca0541d3
skyer9 <skyer9@gmail.com>
2021-09-11 19:51:38 +0900

    bug fix

--- security_group/variables.tf
+++ security_group/variables.tf
@@ -4,6 +4,12 @@ variable "stack_name" {
   default     = "my"
 }
 
+variable "region" {
+  description = "The AWS region to deploy into."
+  type        = string
+  default     = "ap-northeast-2"
+}
+
 variable "allowlist_ip" {
   description = "A list of IP address to grant access via the LBs."
   type        = list(string)
