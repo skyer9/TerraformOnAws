d9acf0511638aeb0c1610f37ed7a0bda16f1f06e
skyer9 <skyer9@gmail.com>
2021-09-11 15:44:20 +0900

    add port range to security group

--- security_group/sg.tf
+++ security_group/sg.tf
@@ -31,8 +31,8 @@ resource "aws_security_group" "consul_lb" {
 
 resource "aws_security_group_rule" "consul_lb_server_ingress" {
   type        = "ingress"
-  from_port   = 8500
-  to_port     = 8500
+  from_port   = 8300
+  to_port     = 8600
   protocol    = "tcp"
   security_group_id = aws_security_group.consul_lb.id
   source_security_group_id = aws_security_group.server_lb.id
@@ -40,8 +40,8 @@ resource "aws_security_group_rule" "consul_lb_server_ingress" {
 
 resource "aws_security_group_rule" "consul_lb_client_ingress" {
   type        = "ingress"
-  from_port   = 8500
-  to_port     = 8500
+  from_port   = 8300
+  to_port     = 8600
   protocol    = "tcp"
   security_group_id = aws_security_group.consul_lb.id
   source_security_group_id = aws_security_group.client_lb.id
@@ -49,8 +49,8 @@ resource "aws_security_group_rule" "consul_lb_client_ingress" {
 
 resource "aws_security_group_rule" "consul_lb_consul_ingress" {
   type        = "ingress"
-  from_port   = 8500
-  to_port     = 8500
+  from_port   = 8300
+  to_port     = 8600
   protocol    = "tcp"
   security_group_id = aws_security_group.consul_lb.id
   source_security_group_id = aws_security_group.consul_lb.id
