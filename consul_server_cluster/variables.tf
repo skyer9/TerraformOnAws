9fbd96a19166b977338346212330c5afccd82520
skyer9 <skyer9@gmail.com>
2021-09-11 19:56:59 +0900

    remove unused variable

--- consul_server_cluster/variables.tf
+++ consul_server_cluster/variables.tf
@@ -40,34 +40,10 @@ variable "consul_server_instance_type" {
   default     = "t3a.micro"
 }
 
-variable "server_instance_type" {
-  description = "The EC2 instance type to launch for Nomad servers."
-  type        = string
-  default     = "t3a.micro"
-}
-
 variable "consul_server_count" {
   description = "The number of Consul servers to run."
   type        = number
   # default     = 1
-}
-
-variable "server_count" {
-  description = "The number of Nomad servers to run."
-  type        = number
-  default     = 1
-}
-
-variable "client_instance_type" {
-  description = "The EC2 instance type to launch for Nomad clients."
-  type        = string
-  default     = "t3a.small"
-}
-
-variable "client_count" {
-  description = "The number of Nomad clients to run."
-  type        = number
-  default     = 1
 }
 
 variable "root_block_device_size" {
@@ -80,16 +56,4 @@ variable "retry_join" {
   description = "The retry join configuration to use."
   type        = string
   default     = "provider=aws tag_key=ConsulAutoJoin tag_value=auto-join"
-}
-
-variable "allowlist_ip" {
-  description = "A list of IP address to grant access via the LBs."
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
-}
-
-variable "nomad_autoscaler_image" {
-  description = "The Docker image to use for the Nomad Autoscaler job."
-  type        = string
-  default     = "hashicorp/nomad-autoscaler:0.3.3"
 }
