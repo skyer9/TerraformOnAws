81e9223ae7c56203a69d304c82a65a8deb8f9570
skyer9 <skyer9@gmail.com>
2021-09-11 12:56:16 +0900

    add domain to elb

--- nomad_server_cluster/domain.tf
+++ nomad_server_cluster/domain.tf
@@ -1,9 +1,9 @@
-resource "aws_route53_zone" "skyer9_pe_kr_zone" {
+data "aws_route53_zone" "skyer9_pe_kr_zone" {
   name = "skyer9.pe.kr"
 }
 
 resource "aws_route53_record" "nomad_server" {
-  zone_id = aws_route53_zone.skyer9_pe_kr_zone.zone_id
+  zone_id = data.aws_route53_zone.skyer9_pe_kr_zone.zone_id
   name    = "skyer9.pe.kr"
   type    = "A"
 
