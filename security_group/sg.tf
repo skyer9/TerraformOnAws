e8a0db032e311bc7bad7248e295355b9c8bad45f
skyer9 <skyer9@gmail.com>
2021-09-11 21:26:34 +0900

    fix sg for consul server

--- security_group/sg.tf
+++ security_group/sg.tf
@@ -83,15 +83,6 @@ resource "aws_security_group" "server_lb" {
   }
 }
 
-resource "aws_security_group_rule" "server_lb_nomad_server_ingress" {
-  type        = "ingress"
-  from_port   = 4646
-  to_port     = 4648
-  protocol    = "tcp"
-  security_group_id = aws_security_group.server_lb.id
-  source_security_group_id = aws_security_group.server_lb.id
-}
-
 resource "aws_security_group_rule" "server_lb_nomad_client_ingress" {
   type        = "ingress"
   from_port   = 4646
@@ -99,15 +90,6 @@ resource "aws_security_group_rule" "server_lb_nomad_client_ingress" {
   protocol    = "tcp"
   security_group_id = aws_security_group.server_lb.id
   source_security_group_id = aws_security_group.client_lb.id
-}
-
-resource "aws_security_group_rule" "server_lb_consul_server_ingress" {
-  type        = "ingress"
-  from_port   = 8300
-  to_port     = 8600
-  protocol    = "tcp"
-  security_group_id = aws_security_group.server_lb.id
-  source_security_group_id = aws_security_group.server_lb.id
 }
 
 resource "aws_security_group_rule" "server_lb_consul_client_ingress" {
@@ -145,6 +127,19 @@ resource "aws_security_group" "client_lb" {
     to_port     = 4648
     protocol    = "tcp"
     cidr_blocks = var.my_ip
+    security_groups = [aws_security_group.server_lb.id]
+  }
+
+  # Consul HTTP API & UI.
+  ingress {
+    from_port   = 8300
+    to_port     = 8600
+    protocol    = "tcp"
+    cidr_blocks = var.my_ip
+    security_groups = [
+      aws_security_group.consul_lb.id,
+      aws_security_group.server_lb.id
+    ]
   }
 
   # Webapp HTTP.
@@ -177,133 +172,6 @@ resource "aws_security_group" "client_lb" {
     to_port     = 4936
     protocol    = "tcp"
     cidr_blocks = var.my_ip
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group_rule" "client_lb_nomad_server_ingress" {
-  type        = "ingress"
-  from_port   = 4646
-  to_port     = 4648
-  protocol    = "tcp"
-  security_group_id = aws_security_group.client_lb.id
-  source_security_group_id = aws_security_group.server_lb.id
-}
-
-resource "aws_security_group_rule" "client_lb_nomad_client_ingress" {
-  type        = "ingress"
-  from_port   = 4646
-  to_port     = 4648
-  protocol    = "tcp"
-  security_group_id = aws_security_group.client_lb.id
-  source_security_group_id = aws_security_group.client_lb.id
-}
-
-resource "aws_security_group_rule" "client_lb_consul_server_ingress" {
-  type        = "ingress"
-  from_port   = 8300
-  to_port     = 8600
-  protocol    = "tcp"
-  security_group_id = aws_security_group.client_lb.id
-  source_security_group_id = aws_security_group.server_lb.id
-}
-
-resource "aws_security_group_rule" "client_lb_consul_client_ingress" {
-  type        = "ingress"
-  from_port   = 8300
-  to_port     = 8600
-  protocol    = "tcp"
-  security_group_id = aws_security_group.client_lb.id
-  source_security_group_id = aws_security_group.client_lb.id
-}
-
-resource "aws_security_group_rule" "client_lb_consul_consul_ingress" {
-  type        = "ingress"
-  from_port   = 8300
-  to_port     = 8600
-  protocol    = "tcp"
-  security_group_id = aws_security_group.client_lb.id
-  source_security_group_id = aws_security_group.consul_lb.id
-}
-
-resource "aws_security_group" "primary" {
-  name   = "${var.stack_name}-primary"
-  vpc_id = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.allowlist_ip
-  }
-
-  # Nomad
-  ingress {
-    from_port       = 4646
-    to_port         = 4646
-    protocol        = "tcp"
-    cidr_blocks     = var.allowlist_ip
-    security_groups = [aws_security_group.server_lb.id]
-  }
-
-  # Consul
-  ingress {
-    from_port       = 8500
-    to_port         = 8500
-    protocol        = "tcp"
-    cidr_blocks     = var.allowlist_ip
-    security_groups = [aws_security_group.server_lb.id]
-  }
-
-  # grafana
-  ingress {
-    from_port       = 3000
-    to_port         = 3000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.client_lb.id]
-  }
-
-  # haproxy
-  ingress {
-    from_port       = 4936
-    to_port         = 4936
-    protocol        = "tcp"
-    security_groups = [aws_security_group.client_lb.id]
-  }
-
-  ingress {
-    from_port       = 9090
-    to_port         = 9090
-    protocol        = "tcp"
-    security_groups = [aws_security_group.client_lb.id]
-  }
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.client_lb.id]
-  }
-
-  # Nomad dynamic port allocation range.
-  ingress {
-    from_port       = 20000
-    to_port         = 32000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.client_lb.id]
-  }
-
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
   }
 
   egress {
