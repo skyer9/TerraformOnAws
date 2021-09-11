569d7b4141b43f1d4ed405aabca38828d7521fc9
skyer9 <skyer9@gmail.com>
2021-09-11 21:34:29 +0900

    fix sg for consul server

--- security_group/sg.tf
+++ security_group/sg.tf
@@ -29,6 +29,15 @@ resource "aws_security_group" "consul_lb" {
   }
 }
 
+resource "aws_security_group_rule" "consul_lb_consul_ingress" {
+  type        = "ingress"
+  from_port   = 8300
+  to_port     = 8600
+  protocol    = "tcp"
+  security_group_id = aws_security_group.consul_lb.id
+  source_security_group_id = aws_security_group.consul_lb.id
+}
+
 resource "aws_security_group_rule" "consul_lb_server_ingress" {
   type        = "ingress"
   from_port   = 8300
