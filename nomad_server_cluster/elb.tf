dd2c1cadaa244bece8e43d8d5160423d803e7fa3
skyer9 <skyer9@gmail.com>
2021-09-12 11:09:38 +0900

    add ssl to nomad server

--- nomad_server_cluster/elb.tf
+++ nomad_server_cluster/elb.tf
@@ -8,7 +8,7 @@ resource "aws_elb" "nomad_server" {
   listener {
     instance_port     = 4646
     instance_protocol = "http"
-    lb_port            = 443
+    lb_port            = 4646
     lb_protocol        = "https"
     ssl_certificate_id = aws_acm_certificate.cert_skyer9_pe_kr.arn
   }
--- nomad_server_cluster/outputs.tf
+++ nomad_server_cluster/outputs.tf
@@ -1,3 +1,3 @@
 output "nomad_addr" {
-  value = "http://${aws_route53_record.nomad_server.name}:4646"
+  value = "https://${aws_route53_record.nomad_server.name}:4646"
 }
