3c8ae2190b7f98ded6d4c47e007e50bc586db2ba
skyer9 <skyer9@gmail.com>
2021-09-12 13:24:15 +0900

    add job

--- nomad_server_cluster/acm.tf
+++ nomad_server_cluster/acm.tf
@@ -1,9 +1,9 @@
 resource "aws_acm_certificate" "cert_nomad_server_skyer9_pe_kr" {
-  domain_name       = "nomad.server.skyer9.pe.kr"
+  domain_name       = "nomad-server.skyer9.pe.kr"
   validation_method = "DNS"
 
   tags = {
-    Environment = "nomad.server.skyer9.pe.kr"
+    Environment = "nomad-server.skyer9.pe.kr"
   }
 
   lifecycle {
