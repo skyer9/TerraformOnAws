5f2c1a916cea91222142bdc2881b63d773353812
skyer9 <skyer9@gmail.com>
2021-09-12 14:04:11 +0900

    create acm first

--- security_group/acm.tf
+++ security_group/acm.tf
@@ -1,10 +1,9 @@
 resource "aws_acm_certificate" "cert_skyer9_pe_kr" {
-  domain_name       = "skyer9.pe.kr"
-  #subject_alternative_names = ["*.skyer9.pe.kr"]
+  domain_name       = "*.skyer9.pe.kr"
   validation_method = "DNS"
 
   tags = {
-    Environment = "*.skyer9.pe.kr"
+    Environment = "skyer9.pe.kr"
   }
 
   lifecycle {
