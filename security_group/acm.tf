0a707541b3233111a5077abc81015874669bb4b7
skyer9 <skyer9@gmail.com>
2021-09-12 13:43:25 +0900

    create domain first

---
+++ security_group/acm.tf
@@ -0,0 +1,12 @@
+resource "aws_acm_certificate" "cert_nomad_server_skyer9_pe_kr" {
+  domain_name       = "nomad-server.skyer9.pe.kr"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "nomad-server.skyer9.pe.kr"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
