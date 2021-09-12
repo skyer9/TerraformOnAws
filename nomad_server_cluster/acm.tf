52246b5d7efeb939b610913ab39b683f5eb677da
skyer9 <skyer9@gmail.com>
2021-09-12 11:06:28 +0900

    add ssl to nomad server

---
+++ nomad_server_cluster/acm.tf
@@ -0,0 +1,12 @@
+resource "aws_acm_certificate" "cert_skyer9_pe_kr" {
+  domain_name       = "*.skyer9.pe.kr"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "skyer9.pe.kr"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
--- nomad_server_cluster/elb.tf
+++ nomad_server_cluster/elb.tf
@@ -8,8 +8,9 @@ resource "aws_elb" "nomad_server" {
   listener {
     instance_port     = 4646
     instance_protocol = "http"
-    lb_port           = 4646
-    lb_protocol       = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = aws_acm_certificate.cert_skyer9_pe_kr.arn
   }
 
   security_groups = [data.aws_security_group.server_lb.id]
